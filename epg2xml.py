56b033f27b954361827394cd9daf8902f73c964e
wiserain <mail275@gmail.com>
2020-08-04 10:03:44 +0900

    1.4.2 - XML invalid unicode char가 들어올 경우 필터링

--- epg2xml.py
+++ epg2xml.py
@@ -12,12 +12,12 @@ from functools import partial
 from urllib.parse import unquote
 from logging.handlers import RotatingFileHandler
 from datetime import datetime, timedelta, date
-from xml.sax.saxutils import escape, unescape
+from xml.sax.saxutils import escape as _escape, unescape
 
 #
 # default variables
 #
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 today = date.today()
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
 req_timeout = 15
@@ -1068,6 +1068,23 @@ def request_data(url, params, method='GET', output='html', session=None, ret='')
         log.error('요청 중 에러: %s' % str(e))
     time.sleep(req_sleep)
     return ret
+
+
+# https://stackoverflow.com/a/22273639
+_illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F), (0x7F, 0x84), (0x86, 0x9F), (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
+if sys.maxunicode >= 0x10000:  # not narrow build
+    _illegal_unichrs.extend([
+        (0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
+        (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF), (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
+        (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+        (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)
+    ])
+_illegal_ranges = ["%s-%s" % (chr(low), chr(high)) for (low, high) in _illegal_unichrs]
+_illegal_xml_chars_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges))
+
+
+def escape(s):
+    return _escape(_illegal_xml_chars_RE.sub(' ', s))
 
 
 Channeldatajson = load_json(args['channelfile'])
