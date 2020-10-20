c807d5505a701f25bff2ccb3b89a92b813c7907f
pinkswall <tmddn741741@gmail.com>
2020-10-20 17:37:10 +0900

    SKB 채널덤프 함수 구현
    Skb에서 제공하는 EPG 채널목록을 파싱하는 함수.

---
+++ DumpChannels/FromSkb.py
@@ -0,0 +1,44 @@
+import json
+import requests
+from datetime import datetime
+
+def DumpChannelsFromSkb():
+  """
+  SKB에서 제공하는 EPG 목록을 파싱합니다. \n
+  @return [
+    {
+      'SKB Name': '채널이름', 
+      'SKBCh': 채널번호,
+      'Source': 'SKB',
+      'ServiceId': '서비스ID'
+    }
+  ] \n
+  @request_count 1
+  """
+
+  UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:81.0) Gecko/20100101 Firefox/81.0'
+  try:
+    req = requests.get("https://m.skbroadband.com/content/realtime/Realtime_List_Ajax.do", headers={'User-Agent': UA})
+    print('Status Code: ', req.status_code)
+  except Exception as e:
+        print('요청 중 에러: %s' % str(e))
+
+  channels = json.loads(req.text)
+  result = []
+
+  for channel in channels:
+    if channel['depth'] == '1':
+      continue
+    ch_name = channel['m_name']
+    ch_no = channel['ch_no']
+    ch_id = channel['c_menu']
+    result.append({
+      'SKB Name': ch_name,
+      'SKBCh': int(ch_no),
+      'Source': 'SKB',
+      'ServiceId': ch_id
+    })
+  
+  headers = [{'last update': datetime.now().strftime('%Y/%m/%d %H:%M:%S'), 'total': len(result)}]
+
+  return headers + result
